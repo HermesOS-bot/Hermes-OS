f696c814a9de63c15fd1bcffe1fa3ddb659a846c
Елена Гуревич <elenagurevic@MacBook-Air-Elena.local>
2026-07-04 20:04:41 +0300

    Add Candle domain model

---
+++ src/core/models.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass(frozen=True)
+class Candle:
+    """
+    Represents a single market candle.
+    """
+
+    timestamp: datetime
+
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: float
